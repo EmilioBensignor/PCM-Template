89a39154129bd71d663328476b6ea3707bef794d
Emilio Bensignor <liobensignor@gmail.com>
2026-02-03 16:49:02 -0300

    PCM - Template

---
+++ app/components/default/Footer.vue
@@ -0,0 +1,5 @@
+<template>
+    <footer class="flex justify-center bg-terciary p-2">
+        <p class="text-xs text-dark font-light">© {{ new Date().getFullYear() }} Tour Experto. Todos los derechos reservados</p>
+    </footer>
+</template>
---
+++ app/components/default/Header.vue
@@ -0,0 +1,12 @@
+<template>
+    <header class="flex justify-center relative bg-secondary p-6 lg:px-24">
+        <HeaderNav />
+        <NuxtLink :to="ROUTE_NAMES.HOME">
+            <NuxtImg src="/images/Logo-Tour-Experto.svg" alt="Logo Tour Experto" class="h-8 lg:h-12" />
+        </NuxtLink>
+    </header>
+</template>
+
+<script setup>
+import { ROUTE_NAMES } from '~/constants/ROUTE_NAMES';
+</script>
